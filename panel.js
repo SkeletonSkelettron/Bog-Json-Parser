8716f68e1220a5697a4705a55f1a6b50d54b6c1b
Misha Sulikashvili <misha.sulikashvili@gmail.com>
2021-10-10 12:48:49 +0400

    final beta

--- panel.js
+++ panel.js
@@ -236,7 +236,29 @@ function copyJson() {
     copyText = arr.find((x) => x.id.toString() === copyId)?.response;
   }
   if (copyText) {
-    navigator.clipboard.writeText(copyText);
+    var textArea = document.createElement("textarea");
+    textArea.style.position = "fixed";
+    textArea.style.top = 0;
+    textArea.style.left = 0;
+    textArea.style.width = "2em";
+    textArea.style.height = "2em";
+    textArea.style.padding = 0;
+    textArea.style.border = "none";
+    textArea.style.outline = "none";
+    textArea.style.boxShadow = "none";
+    textArea.style.background = "transparent";
+    textArea.value = copyText;
+    document.body.appendChild(textArea);
+    textArea.focus();
+    textArea.select();
+    try {
+      var successful = document.execCommand("copy");
+      var msg = successful ? "successful" : "unsuccessful";
+      console.log("Copying text command was " + msg);
+    } catch (err) {
+      console.log("unable to copy");
+    }
+    document.body.removeChild(textArea);
   }
 }
 
