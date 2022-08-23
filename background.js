a4e9439611149ebc7f85e6052a9f3165b66704db
Misha Sulikashvili <misha.sulikashvili@gmail.com>
2022-08-24 00:08:21 +0400

    transition to manifest v3

--- background.js
+++ background.js
@@ -1,49 +1,42 @@
 // Chrome automatically creates a background.html page for this to execute.
 // This can access the inspected page via executeScript
-// 
+//
 // Can use:
 // chrome.tabs.*
 // chrome.extension.*
 
-chrome.extension.onConnect.addListener(function (port) {
+chrome.tabs.onActivated.addListener((port) => {
+  var extensionListener = function (message, sender) {
+    if (message.tabId && message.content) {
+      //Evaluate script in inspectedPage
+      if (message.action === "code") {
+        chrome.scripting.executeScript({
+            target: {tabId: message.tabId},
+            args: [{ code: message.content }],
+          });
+        //Attach script to inspectedPage
+      } else if (message.action === "script") {
+        chrome.scripting.executeScript({
+            target: {tabId: message.tabId},
+            args: [{ file: message.content }],
+          });
 
-    var extensionListener = function (message, sender, sendResponse) {
+        //Pass message to inspectedPage
+      } else {
+        chrome.scripting.executeScript({
+            target: {tabId: message.tabId},
+            args: [ message],
+          });
+      }
 
-        if(message.tabId && message.content) {
+      // This accepts messages from the inspectedPage and
+      // sends them to the panel
+    } 
+  };
 
-                //Evaluate script in inspectedPage
-                if(message.action === 'code') {
-                    chrome.tabs.executeScript(message.tabId, {code: message.content});
+  // Listens to messages sent from the panel
+  chrome.tabs.onActivated.addListener(extensionListener);
 
-                //Attach script to inspectedPage
-                } else if(message.action === 'script') {
-                    chrome.tabs.executeScript(message.tabId, {file: message.content});
+  chrome.tabs.onRemoved.removeListener(extensionListener);
 
-                //Pass message to inspectedPage
-                } else {
-                    chrome.tabs.sendMessage(message.tabId, message, sendResponse);
-                }
-
-        // This accepts messages from the inspectedPage and 
-        // sends them to the panel
-        } else {
-            port.postMessage(message);
-        }
-        sendResponse(message);
-    }
-
-    // Listens to messages sent from the panel
-    chrome.extension.onMessage.addListener(extensionListener);
-
-    port.onDisconnect.addListener(function(port) {
-        chrome.extension.onMessage.removeListener(extensionListener);
-    });
-
-    // port.onMessage.addListener(function (message) {
-    //     port.postMessage(message);
-    // });
-
-});
-chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
-    return true;
 });
